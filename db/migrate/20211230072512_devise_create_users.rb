a50423c49d455d6bf6379d94b5377c0bb90eb828
naramuratomofumi <tomofuminaramura@gmail.com>
2021-12-30 16:32:32 +0900

    userテーブルの作成

--- db/migrate/20211230072512_devise_create_users.rb
+++ db/migrate/20211230072512_devise_create_users.rb
@@ -6,6 +6,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false
+      t.integer :fav_col_id,        null: false
 
       ## Recoverable
       t.string   :reset_password_token
