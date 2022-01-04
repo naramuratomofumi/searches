671cde5176c6d01b071eb3783306b0d40bffc357
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-04 12:58:26 +0900

    細かい修正とrubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :color
-  
+
   validates :nickname, presence: true
-  validates :fav_col_id, numericality: { other_than: 1, message: "が選択されていません"}
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "は半角数字で入力してください"}
+  validates :fav_col_id, numericality: { other_than: 1, message: 'が選択されていません' }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角数字で入力してください' }
 end
