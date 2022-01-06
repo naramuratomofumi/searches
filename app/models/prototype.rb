fcc3b490a040b99d161e4b75fe5db07f98a8c55b
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-06 17:30:56 +0900

    prototypesテーブルに画像ファイルを紐付け

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,6 @@
 class Prototype < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   validates :season_id, numericality: { other_than: 1, message: 'が選択されていません'}
   validates :main_tops_category_id, numericality: { other_than: 1, message: 'が選択されていません'}
