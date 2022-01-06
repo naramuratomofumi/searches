0a2fe642b7ccd83eb132a8124ae07e86b5ba6dd4
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-06 20:46:04 +0900

    tops_categoryのアクティブハッシュを追加

---
+++ app/models/tops_category.rb
@@ -0,0 +1,12 @@
+class TopsCategory < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: 'アウター' },
+    { id: 3, name: 'インナー' },
+    { id: 4, name: 'ワンピース' }
+  ]
+  
+  include ActiveHash::Associations
+  has_many :prototypes
+
+end
