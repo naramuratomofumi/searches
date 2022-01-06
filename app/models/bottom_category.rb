d36e8102ed5c343cdaae67811b66f851e5326a8a
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-06 20:46:41 +0900

    bottom_categoryのアクティブハッシュを追加

---
+++ app/models/bottom_category.rb
@@ -0,0 +1,14 @@
+class BottomCategory < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: 'フルレングス' },
+    { id: 3, name: 'ショートパンツ' },
+    { id: 4, name: 'ロングスカート(膝より下)' },
+    { id: 5, name: 'ショートスカート' },
+    { id: 6, name: 'ミニスカート' },
+  ]
+  
+  include ActiveHash::Associations
+  has_many :prototypes
+
+end
