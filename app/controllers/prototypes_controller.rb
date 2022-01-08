787fea1a7c84fed52bcf268151f2fcc6ca940cff
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-08 22:30:26 +0900

    投稿機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,9 +4,20 @@ class PrototypesController < ApplicationController
   end
 
   def new
-    @prototypes = Prototype.new
+    @prototype = Prototype.new
   end
 
   def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:image, :season_id, :main_tops_col_id, :main_tops_category_id, :tops_category_id, :tops_col_id, :bottom_category_id, :bottom_col_id, :acc_category_id, :acc_col_id, :text).merge(user_id: current_user.id)
   end
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -8,8 +8,6 @@ class Prototype < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-
-
   validates :season_id, numericality: { other_than: 1, message: 'が選択されていません'}
   validates :main_tops_category_id, numericality: { other_than: 1, message: 'が選択されていません'}
   validates :main_tops_col_id, numericality: { other_than: 1, message: 'が選択されていません'}
