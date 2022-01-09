e821f703fc551603f1a69310a50f0b16ed898202
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-09 20:30:15 +0900

    投稿順に表示されるように設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.all.order('created_at DESC')
   end
 
   def new
