5301bd7e8d8078487cb06f198cc82a4f4c03b48f
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-10 16:07:38 +0900

    JavaScriptが機能していたか確認するための記述を削除

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -2,14 +2,10 @@ document.addEventListener('DOMContentLoaded', function(){
   const postForm = document.getElementById('new_prototype');
       const previewList = document.getElementById('previews');
   if (!postForm) return null;
-  console.log("preview.jsが読み込まれました");
 
   const fileField = document.querySelector('input[type="file"][name="prototype[image]"]');
   fileField.addEventListener('change', function(e){
-    console.log("input要素で値の変化が起きた。");
-    console.log(e.target.files[0]);
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
-    console.log(blob);
   });
 });
