fe9fe5b2e57a24f716d4e9b78e6962a99c2ec00e
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-10 16:29:18 +0900

    画像の選択を変更したら前の画像が消えるように設定

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -5,6 +5,10 @@ document.addEventListener('DOMContentLoaded', function(){
 
   const fileField = document.querySelector('input[type="file"][name="prototype[image]"]');
   fileField.addEventListener('change', function(e){
+    const alreadyPreview = document.querySelector('.preview');
+    if (alreadyPreview){
+      alreadyPreview.remove();
+    };
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
     const previewWrapper = document.createElement('div');
