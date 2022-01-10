7c693ba762f717e0210cc6b8e04ea7a5e4b2f784
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-10 15:37:43 +0900

    新規投稿ページと編集ページのみに画像がプレビューされるようにidを指定

---
+++ app/javascript/preview.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const postForm = document.getElementById('new_prototype');
+  if (!postForm) return null;
+  console.log("preview.jsが読み込まれました");
+});
