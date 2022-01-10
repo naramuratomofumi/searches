14b0f893971066c9d3825d80abbfb5b62fdf2cf9
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-10 15:38:34 +0900

    プレビューを表示するためのスペースを取得

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
   const postForm = document.getElementById('new_prototype');
+      const previewList = document.getElementById('previews');
   if (!postForm) return null;
   console.log("preview.jsが読み込まれました");
 });
