6432e7eef158cbf9cfdef1cd3adc5102ae6c512b
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-11 18:21:13 +0900

    prototypeの単体テストコードのexampleを洗い出し

--- spec/models/prototype_spec.rb
+++ spec/models/prototype_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Prototype, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '投稿機能' do
+    context '投稿ができる時' do
+      it 'image,season_id,main_tops_category_id,main_tops_col_id,tops_category_id,tops_col_id,bottom_category_id,bottom_color_id,acc_category_id,acc_col_id,text,user_idの12項目が存在すれば投稿できる' do
+      end
+      it 'tops_category_idが空でも登録できる' do
+      end
+      it 'tops_col_idが空でも登録できる' do
+      end
+      it 'bottom_category_idが空でも登録できる' do
+      end
+      it 'bottom_color_idが空でも登録できる' do
+      end
+      it 'acc_category_idが空でも登録できる' do
+      end
+      it 'acc_col_idが空でも登録できる' do
+      end
+    end
+
+    context '投稿できない時' do
+      it 'imageが空の時' do
+      end
+      it 'main_tops_category_idが空の時' do
+      end
+      it 'main_tops_col_idが空の時' do
+      end
+      it 'seasonが空の時' do
+      end
+      it 'textが空の時' do
+      end
+      it 'user_idが空の時' do
+      end
+    end
+  end
 end
