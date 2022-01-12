41a1f062784e2e465f306e33ce4b98a41391f235
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-12 19:55:16 +0900

    アソシエーションを組んでいるカラムはバリデーションをかける必要がないので削除

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -12,6 +12,5 @@ class Prototype < ApplicationRecord
   validates :main_tops_category_id, numericality: { other_than: 1, message: 'が選択されていません'}
   validates :main_tops_col_id, numericality: { other_than: 1, message: 'が選択されていません'}
   validates :text, presence: true
-  validates :user_id, presence: true
   validates :image, presence: true
 end
