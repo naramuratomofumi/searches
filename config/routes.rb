243fb46e163427ca458a077564feb8a06e3dfa99
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-13 21:48:37 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
-  resources :prototypes, only:[ :new, :create, :show, :edit, :destroy, :update]
+  resources :prototypes, only:[ :new, :create, :show, :edit, :destroy, :update] do
+    resources :likes, only: [:create, :destroy]
+  end
 end
