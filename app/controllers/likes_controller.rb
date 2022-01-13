18f1b4ff7dc143d50bc3f4f265f39ccc66a1a58a
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-13 21:54:34 +0900

    likesコントローラにアクションを追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,13 @@
 class LikesController < ApplicationController
 
   def create
+    @like = current_user.likes.create(prototype_id: params[:post_id])
+    redirect_back(fallback_location: root_path)
   end
 
-  def destoroy
+  def destroy
+    @like = Like.find_by(prototype_id: params[:prototype_id], user_id: current_user.id)
+    @like.destroy 
+    redirect_back(fallback_location: root_path)
   end
 end
