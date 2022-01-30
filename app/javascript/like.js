7138547426791837355c135bca6c0fe0dfb8d32c
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-30 17:37:06 +0900

    修正

--- app/javascript/like.js
+++
@@ -1,9 +0,0 @@
-function like (){
-  const submit = document.getElementById("like_btn");
-  submit.addEventListener("click", () =>{
-    console.log("クリック時発火") 
-    
-  });
-}
-
-window.addEventListener('load',like);
