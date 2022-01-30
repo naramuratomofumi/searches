ffed0f831a28c344e16edcef04c566576f92d986
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-30 17:35:18 +0900

    コントローラーの修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,13 @@
 class LikesController < ApplicationController
   def create
+    @prototype = Prototype.find(params[:prototype_id])
     @like = current_user.likes.create(prototype_id: params[:prototype_id])
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    @like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
+    @prototype = Prototype.find(params[:prototype_id])
+    @like = current_user.likes.find_by(prototype_id: @prototype.id)
     @like.destroy
     redirect_back(fallback_location: root_path)
   end
